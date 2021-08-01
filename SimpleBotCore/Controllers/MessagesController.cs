8426b5039f5ce653c24ae71ad8631bb0e645e851
Fabricio Catae <fcatae@hotmail.com>
2021-08-01 15:35:27 -0300

    add Messages controller

---
+++ SimpleBotCore/Controllers/MessagesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBotCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessagesController : ControllerBase
+    {
+        private readonly ILogger<MessagesController> _logger;
+
+        public MessagesController(ILogger<MessagesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Simple Bot está online";
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
